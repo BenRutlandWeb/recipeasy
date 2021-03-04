0f6915c64e0ad9d6ea4d891e9f85d2af80d7e73f
BenRutlandWeb <benrutland@hotmail.com>
2021-03-04 21:36:36 +0000

    Deploying to gh-pages from @ BenRutlandWeb/recipeasy@6dab5a0f7897dc5c780cdc529f56208064744ca8 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https:/benrutlandweb.github.io/recipeasy/sw.js', { scope: 'https://benrutlandweb.github.io/recipeasy/' })})}
