d199e082996378a9326f9ad54a05b26d7b8d9979
Ben Rutland <benrutland@hotmail.com>
2021-03-08 11:25:54 +0000

    updated scope for manifest

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
-    return { x: 0, y: 0 };
     return savedPosition || { x: 0, y: 0 };
   },
 });
