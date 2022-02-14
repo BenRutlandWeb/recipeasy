6c0f67ec411c640ff73126220950e7942bbc3fca
Ben Rutland <benrutland@hotmail.com>
2022-02-14 22:34:44 +0000

    copyright year

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
   <footer class="text-center py-8 px-6 border-t dark:border-gray-700">
-    <slot>© 2021 Ben Rutland Web</slot>
+    <slot>© 2021 — 2022 Ben Rutland Web</slot>
   </footer>
 </template>
