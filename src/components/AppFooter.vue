91afe9462abaca1084c7b959f70e683e5852e47e
Ben Rutland <benrutland@hotmail.com>
2022-02-14 22:38:13 +0000

    updated packages

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,9 @@
 <template>
   <footer class="text-center py-8 px-6 border-t dark:border-gray-700">
-    <slot>© 2021 — 2022 Ben Rutland Web</slot>
+    <slot>© 2021 - {{ currentYear }} Ben Rutland Web</slot>
   </footer>
 </template>
+
+<script setup>
+const currentYear = new Date().getFullYear();
+</script>
