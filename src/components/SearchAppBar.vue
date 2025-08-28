87b8d0ed1c252e347149ba315e9e2120ea6e6155
Ben Rutland <benrutland@hotmail.com>
2025-08-28 22:09:43 +0100

    fixed search

--- src/components/SearchAppBar.vue
+++ src/components/SearchAppBar.vue
@@ -1,21 +1,23 @@
 <template>
-    <AppBarButton @click="$router.back()" aria-label="Go home">
-        <BaseIcon icon="arrow_back" />
-    </AppBarButton>
-    <label for="app-search" class="sr-only"> Search by ingredient, dish, keyword... </label>
-    <input
-        type="search"
-        id="app-search"
-        placeholder="Ingredient, dish, keyword..."
-        autocomplete="off"
-        :value="query"
-        @input="search"
-        ref="appSearch"
-        class="text-xl flex-1 placeholder-gray-500 focus:outline-none bg-transparent"
-    />
-    <AppBarButton @click="clear" v-if="query" aria-label="clear search">
-        <BaseIcon icon="close" />
-    </AppBarButton>
+  <AppBarButton @click="$router.back()" aria-label="Go home">
+    <BaseIcon icon="arrow_back" />
+  </AppBarButton>
+  <label for="app-search" class="sr-only">
+    Search by ingredient, dish, keyword...
+  </label>
+  <input
+    type="search"
+    id="app-search"
+    placeholder="Ingredient, dish, keyword..."
+    autocomplete="off"
+    :value="query"
+    @input="search"
+    ref="appSearch"
+    class="text-xl flex-1 placeholder-gray-500 focus:outline-none bg-transparent"
+  />
+  <AppBarButton @click="clear" v-if="query" aria-label="clear search">
+    <BaseIcon icon="close" />
+  </AppBarButton>
 </template>
 
 <script setup>
@@ -28,18 +30,18 @@ const query = computed(() => route.query.q || "");
 const appSearch = ref(null);
 
 function search({ target }) {
-    router.push({
-        name: "search",
-        query: Object.assign({}, route.query, { q: target.value }),
-    });
+  router.replace({
+    name: "search",
+    query: Object.assign({}, route.query, { q: target.value }),
+  });
 }
 function clear({ target }) {
-    const newQuery = Object.assign({}, route.query);
-    delete newQuery.q;
-    router.push({
-        query: newQuery,
-    });
-    appSearch.value.focus();
+  const newQuery = Object.assign({}, route.query);
+  delete newQuery.q;
+  router.replace({
+    query: newQuery,
+  });
+  appSearch.value.focus();
 }
 onMounted(() => appSearch.value.focus());
 </script>
