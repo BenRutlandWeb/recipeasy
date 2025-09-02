8b07387059cb2cd0c86bf2aee493599a3ea91e05
Ben Rutland <benrutland@hotmail.com>
2025-09-02 19:21:14 +0100

    added spag bol

--- src/IndexRecipes.js
+++ src/IndexRecipes.js
@@ -3,87 +3,99 @@ import path from "path";
 import { watch } from "chokidar";
 
 function reduceWords(words) {
-    return words.filter((w, i, arr) => !arr.some((other, j) => i !== j && other.includes(w)));
+  return words.filter(
+    (w, i, arr) => !arr.some((other, j) => i !== j && other.includes(w))
+  );
 }
 
 function splitString(words) {
-    return words
-        .toLowerCase()
-        .split(/[^a-zA-Z]+/)
-        .filter(Boolean);
+  return words
+    .toLowerCase()
+    .split(/[^a-zA-Z]+/)
+    .filter(Boolean);
 }
 
 function buildSearchIndex() {
-    const recipesDir = path.resolve(__dirname, "./data/recipes");
-    const files = fs.readdirSync(recipesDir).filter((f) => f.endsWith(".json"));
+  const recipesDir = path.resolve(__dirname, "./data/recipes");
+  const files = fs.readdirSync(recipesDir).filter((f) => f.endsWith(".json"));
 
-    const output = {};
+  const output = {};
 
-    files.forEach((f) => {
-        const content = JSON.parse(fs.readFileSync(path.join(recipesDir, f), "utf-8"));
-        const slug = f.replace(/\.json$/, "");
+  files.forEach((f) => {
+    const content = JSON.parse(
+      fs.readFileSync(path.join(recipesDir, f), "utf-8")
+    );
+    const slug = f.replace(/\.json$/, "");
 
-        const keywords = reduceWords([
-            ...new Set([
-                ...splitString(content.title),
-                ...content.ingredients.map((i) => splitString(i.name)).flat(),
-                ...content.keywords.map((i) => splitString(i)).flat(),
-            ]),
-        ]);
+    const keywords = reduceWords([
+      ...new Set([
+        ...splitString(content.title),
+        ...content.ingredients.map((i) => splitString(i.name)).flat(),
+        ...content.keywords.map((i) => splitString(i)).flat(),
+      ]),
+    ]);
 
-        keywords.forEach((word) => {
-            if (!output[word]) {
-                output[word] = [];
-            }
+    keywords.forEach((word) => {
+      if (!output[word]) {
+        output[word] = [];
+      }
 
-            output[word].push(slug);
-        });
+      output[word].push(slug);
     });
+  });
 
-    //const reduced = reduceWords(output);
+  //const reduced = reduceWords(output);
 
-    fs.writeFileSync(path.resolve(__dirname, "./data/recipes-search.json"), JSON.stringify(output, null, 2));
+  fs.writeFileSync(
+    path.resolve(__dirname, "./data/recipes-search.json"),
+    JSON.stringify(output, null, 2)
+  );
 }
 
 function buildListings() {
-    const recipesDir = path.resolve(__dirname, "./data/recipes");
-    const files = fs.readdirSync(recipesDir).filter((f) => f.endsWith(".json"));
+  const recipesDir = path.resolve(__dirname, "./data/recipes");
+  const files = fs.readdirSync(recipesDir).filter((f) => f.endsWith(".json"));
 
-    const output = {};
+  const output = {};
 
-    files.forEach((f) => {
-        const content = JSON.parse(fs.readFileSync(path.join(recipesDir, f), "utf-8"));
-        const slug = f.replace(/\.json$/, "");
+  files.forEach((f) => {
+    const content = JSON.parse(
+      fs.readFileSync(path.join(recipesDir, f), "utf-8")
+    );
+    const slug = f.replace(/\.json$/, "");
 
-        output[slug] = {
-            title: content.title,
-            image: content.image,
-        };
-    });
+    output[slug] = {
+      title: content.title,
+      image: content.image,
+    };
+  });
 
-    fs.writeFileSync(path.resolve(__dirname, "./data/recipes.json"), JSON.stringify(output, null, 2));
+  fs.writeFileSync(
+    path.resolve(__dirname, "./data/recipes.json"),
+    JSON.stringify(output, null, 2)
+  );
 }
 
 export default function indexRecipes() {
-    let watcher;
+  let watcher;
 
-    return {
-        name: "recipes-manifest",
-        buildStart() {
-            watcher = watch(__dirname, "./data/recipes/*.json");
+  return {
+    name: "recipes-manifest",
+    buildStart() {
+      watcher = watch(__dirname, "./data/recipes/*.json");
 
-            watcher.on("add", (file) => {
-                buildListings();
-                buildSearchIndex();
-            });
-            watcher.on("change", (file) => {
-                buildListings();
-                buildSearchIndex();
-            });
-        },
+      watcher.on("add", (file) => {
+        buildListings();
+        buildSearchIndex();
+      });
+      watcher.on("change", (file) => {
+        buildListings();
+        buildSearchIndex();
+      });
+    },
 
-        closeBundle() {
-            watcher?.close();
-        },
-    };
+    closeBundle() {
+      watcher?.close();
+    },
+  };
 }
