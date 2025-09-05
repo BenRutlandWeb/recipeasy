08c7e7beb6828fe8e6ea39b8822fe93386bab778
Ben Rutland <benrutland@hotmail.com>
2025-09-05 16:00:10 +0100

    removed scrollbar

--- src/components/SliderGroup.vue
+++ src/components/SliderGroup.vue
@@ -1,32 +1,33 @@
 <template>
-    <component :is="tag" class="slider-group flex overflow-auto px-6 pb-8 -mx-6">
-        <template v-for="item in items">
-            <li :key="item" class="slider-item" v-if="item">
-                <slot :item="item">{{ item }}</slot>
-            </li>
-        </template>
-    </component>
+  <component :is="tag" class="slider-group flex overflow-auto px-6 pb-8 -mx-6">
+    <template v-for="item in items">
+      <li :key="item" class="slider-item" v-if="item">
+        <slot :item="item">{{ item }}</slot>
+      </li>
+    </template>
+  </component>
 </template>
 
 <script setup>
 const props = defineProps({
-    items: {
-        type: Array,
-    },
-    tag: {
-        type: String,
-        default: "ul",
-    },
+  items: {
+    type: Array,
+  },
+  tag: {
+    type: String,
+    default: "ul",
+  },
 });
 </script>
 
 <style scoped>
 .slider-group {
-    scroll-snap-type: x mandatory;
+  scroll-snap-type: x mandatory;
+  scrollbar-width: none;
 }
 .slider-item {
-    scroll-snap-align: start;
-    scroll-margin-inline-start: 1.5rem;
-    flex: 0 0 min(360px, 36vw);
+  scroll-snap-align: start;
+  scroll-margin-inline-start: 1.5rem;
+  flex: 0 0 min(360px, 36vw);
 }
 </style>
