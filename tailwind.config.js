e493793afe01e1baf48fbb658b7f9489cb9b3fa8
Ben Rutland <ben@andanotherday.com>
2026-09-17 14:57:19 +0100

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,vue,js,ts}'],
+  content: ['./src/**/*.{astro,vue,ts}'],
   darkMode: 'class',
   theme: {
     extend: {
